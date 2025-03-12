672bdb7319808a4099a5dd8c092ea824e9c1df78
Timur Bairamgulov <jesorstaff@gmail.com>
2025-03-12 20:44:51 +0500

    add footer

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,60 @@
+<template>
+    <footer class="bg-gray-dark w-full py-[55px]">
+        <div class="container m-auto">
+            <div class="grid grid-cols-4 gap-4">
+                <div>
+                    <h3 class="text-white text-2xl font-bold">Flower Shop</h3>
+                    <p class="text-white text-sm">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quisquam, quos.
+                    </p>
+                </div>
+                <div>
+                    <h4 class="text-white text-lg font-bold">Links</h4>
+                    <ul class="text-white">
+                        <li>
+                            <RouterLink to="/">Home</RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="/">Home</RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="/">Home</RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="/">Home</RouterLink>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 class="text-white text-lg font-bold">Links</h4>
+                    <ul class="text-white">
+                        <li>
+                            <RouterLink to="/">Home</RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="/">Home</RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="/">Home</RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="/">Home</RouterLink>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 class="text-white text-2xl font-bold">Contact</h4>
+                    <ul class="text-white">
+                        <li>
+                            <RouterLink to="/">Home</RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="/">Home</RouterLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import Header from '@/components/Header/Header.vue'
+import Footer from '@/components/Footer/Footer.vue'
 </script>
 
 <template>
     <Header />
     <slot></slot>
     <RouterView />
+    <Footer />
 </template>
